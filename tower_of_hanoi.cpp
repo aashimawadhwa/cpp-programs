f34b7bc15d0ba97e9d14ab7e6138a40ec7baa3cd
Ankur rai <rai.ankur0007@gmail.com>
2021-03-25 00:19:02 +0530

    Removing extra Middle call of function
    Although this will not reduce the no of a function call, it is a convenient way to understand, toh2 will call the function on n == 0 whereas your's is calling on n == 1.

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -10,10 +10,21 @@ void toh(int n,char S,char D, char T){
         toh(n-1,T,D,S);
     }
 }
+
+void toh2(int n,char S,char D, char T){
+    if(n==0){
+        return;
+    }
+    toh2(n-1,S,T,D);
+    cout<<"moved "<< n <<"'th disk from "<<S<<"  to --> "<<D<<endl;
+    toh2(n-1,T,D,S);
+}
+
 int main() {
     int x;
     char a='A',b='C',c='B';
     cin>>x;
     toh(x,a,b,c);
+    toh2(x,a,b,c);
     return 0;
 }
